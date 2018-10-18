910409382ab4234a9c6249d524b2c1e027fea3ac
мой номер <del.exe@gmail.com>
2018-10-18 09:36:15 +0200

    Initial Commit

---
+++ GiphyClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphyClient
+//
+//  Created by DEL on 18.10.2018.
+//  Copyright © 2018 DEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
